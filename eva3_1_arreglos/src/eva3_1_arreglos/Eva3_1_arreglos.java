6f074c3dbadbd570bd5e3b1b85682f14b3ac0f58
gene ricardo lazarin nuñez <159508631+genelazarin86@users.noreply.github.com>
2024-04-22 16:51:05 -0600

    Add files via upload

---
+++ eva3_1_arreglos/src/eva3_1_arreglos/Eva3_1_arreglos.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva3_1_arreglos;
+
+/**
+ *
+ * @author invitado
+ */
+public class Eva3_1_arreglos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int arreglo[] = new int[10];
+        arreglo[1]=100;
+        System.out.println("valor de arreglo[1] = "+arreglo[1]);
+        System.out.println(arreglo.length);
+    }
+    
+}
