18846aaa67d6c74508b90c75353b7fffb88aeb85
gene ricardo lazarin nuñez <159508631+genelazarin86@users.noreply.github.com>
2024-05-02 16:53:07 -0600

    Add files via upload

---
+++ EVA3_10_for_each/src/eva3_10_for_each/EVA3_10_for_each.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva3_10_for_each;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA3_10_for_each {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String diasSemana[]={"domingo","lunes","martes","miercoles","jueves","viernes","sabado"};
+        for (String string : diasSemana) {
+            System.out.println("");
+            
+        }
+ {   int salario[]={50,1000,5687,12314,7855,10000,35789};
+ for (int cant : salario) 
+         System.out.println("$"+cant);
+ 
+ {
+         
+     }
+            
+        }
+{
+         
+        }
+    }
+    
+}
